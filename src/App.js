5f5a11e35482bdd25b52b9387ba899a863968ff9
Eben-Success <aebenezer237@gmail.com>
2022-05-01 22:23:45 +0000

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
     return `${day} ${date} ${month} ${year}`    //return the full date and display it
   }
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'} >
       <main>
         <div className="search-box">
         <input 
@@ -53,9 +53,9 @@ function App() {
                     <div className="date">{dateBuilder(new Date())}</div>
                     <div className="weather-box">
                         <div className="temp">
-                            15°C
+                            {Math.round(weather.main.temp)}°C
                         </div>
-                        <div className="weather">Sunny</div>
+                        <div className="weather">{weather.weather[0].main} </div>
                     </div>
                 </div>
             </div>
